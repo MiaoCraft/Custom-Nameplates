c36e430f0733023c3bfeff651753aab312ff168c
泠辰 <93849805+Crsuh2er0@users.noreply.github.com>
2026-08-20 18:19:14 +0800

    Update versionBanner and builder functions to static values
    Replaced dynamic version and builder functions with static 'Unknown' values.

--- build.gradle.kts
+++ build.gradle.kts
@@ -32,10 +32,6 @@ subprojects {
     }
 }
 
-fun versionBanner(): String = project.providers.exec {
-    commandLine("git", "rev-parse", "--short=8", "HEAD")
-}.standardOutput.asText.map { it.trim() }.getOrElse("Unknown")
+fun versionBanner(): String = "Unknown"
 
-fun builder(): String = project.providers.exec {
-    commandLine("git", "config", "user.name")
-}.standardOutput.asText.map { it.trim() }.getOrElse("Unknown")
+fun builder(): String = "Unknown"
